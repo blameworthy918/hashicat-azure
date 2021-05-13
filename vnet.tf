2ca08a572a65037255f32120a57e31248e34a7d8
blame <nfischer@gmail.com>
2021-05-13 18:36:46 +0000

    Added vnet module #2

--- vnet.tf
+++ vnet.tf
@@ -4,15 +4,5 @@ module "network" {
   source  = "app.terraform.io/Blame-training/network/azurerm"
   resource_group_name = azurerm_resource_group.myresourcegroup.location
   version = "3.3.0"
-  address_space       = "10.0.0.0/16"
-  subnet_prefixes     = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
-  subnet_names        = ["subnet1", "subnet2", "subnet3"]
-
-  tags = {
-    environment = "dev"
-    costcenter  = "it"
-  }
-
-  depends_on = [azurerm_resource_group.myresourcegroup.location]
 }
 
