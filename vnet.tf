94a2905a8a0aa305f6e41f418c81680f7f7b9b46
blame <nfischer@gmail.com>
2021-05-13 18:53:22 +0000

    Added vnet module #5

--- vnet.tf
+++ vnet.tf
@@ -2,7 +2,7 @@
 
 module "network" {
   source  = "app.terraform.io/Blame-training/network/azurerm"
-  resource_group_name = azurerm_resource_group.myresourcegroup.location
+  resource_group_name = "blame-workshop"
   version = "3.3.0"
 }
 
